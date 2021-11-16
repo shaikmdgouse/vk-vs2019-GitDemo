d438590baab3fc185727dc07888f368d88be9725
Gouse <shaikmdgouse1997@gmail.com>
2021-11-16 12:14:00 +0530

    Welcome Added

--- vk-vs2019-GitDemo/Startup.cs
+++ vk-vs2019-GitDemo/Startup.cs
@@ -33,6 +33,7 @@ namespace vk_vs2019_GitDemo
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Welcome!");
                 });
             });
         }
